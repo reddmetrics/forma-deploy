ccdad828ce46ad2d726a35b1ff75391f73e21b53
robinkraft <rkraft4@gmail.com>
2012-03-04 14:11:37 -0800

    Updated cl argument indices for messages

--- src/forma/deploy.sh
+++ src/forma/deploy.sh
@@ -16,10 +16,10 @@ echo "lein uberjar"
 lein uberjar
 
 # move jar file to job tracker
-echo "Copying uberjar to $2"
+echo "Copying uberjar to $3"
 scp -i $2 $1/forma-0.2.0-SNAPSHOT-standalone.jar hadoop@$3:
 
-echo "Logging into $2"
+echo "Logging into $3"
 # launch a new screen with logging enabled
 ssh -i $2 hadoop@$3
 
